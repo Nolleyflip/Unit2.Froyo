885b8441cea0f19f6d2bcbbb2517c6a30d4f3153
Nolleyflip <tnunaley@gmail.com>
2024-06-12 19:16:39 -0700

    readable code

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputMessage = prompt(
+    "Please select a flavor of Frozen Yogurt",
+    "Vanilla,Vanilla,Vanilla,Strawberry,Coffee,Coffee"
+);
+
+let flavorList = ["Vanilla","Vanilla","Vanilla","Strawberry","Coffee","Coffee"]
+
+console.log(flavorList)
+
+for(let i = 0; i < flavorList.length; i++){
+//    let flavors = flavors
+} 
